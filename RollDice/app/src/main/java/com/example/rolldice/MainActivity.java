4c043a13c815db00a22bea2711bd5a6970382aa8
abdurrahimagca <abbdrhm@gmail.com>
2023-03-30 16:07:59 +0300

    week2

--- RollDice/app/src/main/java/com/example/rolldice/MainActivity.java
+++ RollDice/app/src/main/java/com/example/rolldice/MainActivity.java
@@ -22,15 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.next_button);
         WebView webView;
-        // initialise the layout
+
         webView = findViewById(R.id.web_view);
 
-        // enable the javascript to load the url
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
-        // add the url of gif
-        //https://i.pinimg.com/originals/0f/ba/e2/0fbae2bd3a0bd45b0d6a25f6459d95a3.gif
+
 
         webView.loadUrl("https://i.pinimg.com/originals/a7/60/f5/a760f5b77dbafdf85c209b642af08b6e.gif");
         button.setOnClickListener(new View.OnClickListener() {
